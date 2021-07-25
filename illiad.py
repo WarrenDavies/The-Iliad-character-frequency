aaf6073c0cfe9a4b64d5104baa2237e700cb4944
W <warren@warrendaviesdev.com>
2021-07-25 08:56:42 +0100

    books, character list, file import

---
+++ illiad.py
@@ -0,0 +1,57 @@
+characters = [ \
+    'Zeus', \
+    'Hera', \
+    
+    'Pallas', \
+    'Athene', \
+    
+    'Aphrodite', \
+    'Poseidon', \
+    'Ares', \
+    'Hephaestus', \
+
+    'Achilles', \
+    'Agamemnon', \
+    'Ajax the lesser', \
+    'Ajax the greater', \
+    'Antilochus', \
+    'Achilles', \
+    'Agamemnon', \
+    #'Ajax or Aias (Αίας), also known as Telamonian Ajax (he was the son of Telamon) and Greater Ajax', \
+    #'Ajax the Lesser', \
+    'Aias'
+    'Antilochus', \
+    'Calchas', \
+    'Diomedes', \
+    'Helen', \
+    'Idomeneus', \
+    'Menelaus', \
+    'Nestor', \
+    'Odysseus', \
+    'Patroclus', \
+    'Phoenix', \
+    'Teucer', \
+    'Aesymnus', \
+
+    #trojans
+    'Aeneas', \
+    'Agenor', \
+    'Andromache', \
+    'Antenor', \
+    'Cassandra', \
+    'Glaucus', \
+    'Hector', \
+    'Laodice', \
+    'Lycaon', \
+    'Pandarus', \
+    'Paris', \
+    'Polydamas', \
+    'Priam', \
+    'Sarpedon', \
+    'Theano', \
+]
+
+with open('book1.txt') as b:
+    book1 = b.read()
+
+    
