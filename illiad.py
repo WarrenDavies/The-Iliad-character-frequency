0856dd4f62e46794c05527df94c135dd543b00ae
W <warren@warrendaviesdev.com>
2021-07-25 14:48:05 +0100

    class version

--- illiad.py
+++ illiad.py
@@ -1,88 +1,80 @@
 import os #, pandas as pd
 
-class character:
-    def __init__(name, group, counts):
-        self.name = name
-        self.group = group
-        self.counts = []
+class Character:
+    def __init__(self, name, group, counts):
+      self.name = name
+      self.group = group
+      self.counts = counts
 
 characters = [ \
-
     # Deities
     #########
-    'Zeus', \
-    'Hera', \
-    'Athene', \
-    'Aphrodite', \
-    'Poseidon', \
-    'Ares', \
-    # 'Hephaestus', \
-    'Hephaistos', \
-    'Apollo', \
-    # 'Hermes', \
-    # 'Thetis', \
+    Character('Zeus', "Deity", []), \
+    Character('Hera', "Deity", []), \
+    Character('Athene', "Deity", []), \
+    Character('Aphrodite', "Deity", []), \
+    Character('Poseidon', "Deity", []), \
+    Character('Ares', "Deity", []), \
+    # Character('Hephaestus', "Deity", []), \
+    Character('Hephaistos', "Deity", []), \
+    Character('Apollo', "Deity", []), \
+    # Character('Hermes', "Deity", []), \
+    # Character('Thetis', "Deity", []), \
 
     # Greeks
     ########
-    'Achilles', \
-    'Agamemnon', \
-    # 'Antilochus', \
-    #'Ajax the greater', # not used in Lang \
-    #'Ajax the Lesser', # not used in Lang \
-    'Aias', # Lang spelling of Ajax \ 
-    # 'Antilochus', \
-    # 'Calchas', \
-    # 'Diomedes', \
-    'Helen', \
-    # 'Idomeneus', \
-    # 'Menelaus', \ # not used in Lang 
-    'Menelaos', \
-    'Nestor', \
-    'Odysseus', \
-    'Patroklos', \
-    # 'Phoenix', \
-    # 'Teucer', \
-    # 'Aesymnus', \
+    Character('Achilles', "Greek", []), \
+    Character('Agamemnon', "Greek", []), \
+    #Character('Antilochus', "Greek", []), \
+    #Character('Ajax the greater', "Greek", []), # not used in Lang \
+    #Character('Ajax the Lesser', "Greek", []), # not used in Lang \
+    Character('Aias', "Greek", []), # Lang spelling of Ajax \ 
+    # Character('Antilochus', "Greek", []), \
+    # Character('Calchas', "Greek", []), \
+    # Character('Diomedes', "Greek", []), \
+    Character('Helen', "Greek", []), \
+    # Character('Idomeneus', "Greek", []), \
+    # Character('Menelaus', "Greek", []), \ # not used in Lang 
+    Character('Menelaos', "Greek", []), \
+    Character('Nestor', "Greek", []), \
+    Character('Odysseus', "Greek", []), \
+    Character('Patroklos', "Greek", []), \
+    # Character('Phoenix', "Greek", []), \
+    # Character('Teucer', "Greek", []), \
+    # Character('Aesymnus', "Greek", []), \
 
     # Trojans
     #########
-    # 'Aeneas', \ # not used in Lang 
-    # 'Aineias', \ 
-    # 'Agenor', \
-    # 'Andromache', \
-    # 'Antenor', \
-    # 'Cassandra', \
-    # 'Glaucus', \
-    'Hector', \
-    # 'Laodice', \
-    # 'Lycaon', \
-    # 'Pandarus', \
-    'Paris', \
-    # 'Polydamas', \
-    'Priam', \
-    # 'Sarpedon', \
-    # 'Theano', \
-    # 'Briseis', \
+    # Character('Aeneas', "Trojan", []), \ # not used in Lang 
+    # Character('Aineias', "Trojan", []), \ 
+    # Character('Agenor', "Trojan", []), \
+    # Character('Andromache', "Trojan", []), \
+    # Character('Antenor', "Trojan", []), \
+    # Character('Cassandra', "Trojan", []), \
+    # Character('Glaucus', "Trojan", []), \
+    Character('Hector', "Trojan", []), \
+    # Character('Laodice', "Trojan", []),  \
+    # Character('Lycaon', "Trojan", []),  \
+    # Character('Pandarus', "Trojan", []),  \
+    Character('Paris', "Trojan", []),  \
+    # Character('Polydamas', "Trojan", []),  \
+    Character('Priam', "Trojan", []),  \
+    # Character('Sarpedon', "Trojan", []),  \
+    # Character('Theano', "Trojan", []),  \
+    # Character('Briseis', "Trojan", []),  \
 ]
-
-counts = []
-for character in characters:
-    counts.append([character])
 
 books = os.listdir('./lang')
 books = sorted(books)
-print(books)
-
 
 for book in books:
     with open('./lang/' + book) as b:
         current_book = b.read()
-    i = 0
     for character in characters:
-        counts[i].append(current_book.count(character))   
-        i = i + 1
+        character.counts.append(current_book.count(character.name))   
 
-for character in counts:
-    for count in character:
+for character in characters:
+    print(character.name + ',' + character.group + ',', end='')
+    for count in character.counts:
         print(str(count) + ",", end='')
     print('')
