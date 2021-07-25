70b8d0a5ae07eba6a9cd3626ec7e73032c7aefdc
W <warren@warrendaviesdev.com>
2021-07-25 19:50:40 +0100

    clean up character list

--- illiad.py
+++ illiad.py
@@ -11,57 +11,53 @@ characters = [ \
     #########
     Character('Zeus', "Deity", []), \
     Character('Hera', "Deity", []), \
-    Character('Athene', "Deity", []), \
+    Character('Athene', "Deity", []), # Athena, Pallas Athene/a \
     Character('Aphrodite', "Deity", []), \
     Character('Poseidon', "Deity", []), \
     Character('Ares', "Deity", []), \
-    # Character('Hephaestus', "Deity", []), \
-    Character('Hephaistos', "Deity", []), \
+    Character('Hephaistos', "Deity", []), # Hephaestus \
     Character('Apollo', "Deity", []), \
-    # Character('Hermes', "Deity", []), \
-    # Character('Thetis', "Deity", []), \
+    Character('Hermes', "Deity", []), \
+    Character('Thetis', "Deity", []), \
+    Character('Dawn', "Deity", []), # Eos \
+    Character('Iris', "Deity", []), \
 
     # Greeks
     ########
     Character('Achilles', "Greek", []), \
     Character('Agamemnon', "Greek", []), \
-    #Character('Antilochus', "Greek", []), \
-    #Character('Ajax the greater', "Greek", []), # not used in Lang \
-    #Character('Ajax the Lesser', "Greek", []), # not used in Lang \
-    Character('Aias', "Greek", []), # Lang spelling of Ajax \ 
-    # Character('Antilochus', "Greek", []), \
-    # Character('Calchas', "Greek", []), \
-    # Character('Diomedes', "Greek", []), \
+    Character('Antilochus', "Greek", []), \
+    Character('Aias', "Greek", []), # Ajax \ 
+    Character('Calchas', "Greek", []), \
+    Character('Diomedes', "Greek", []), \
     Character('Helen', "Greek", []), \
-    # Character('Idomeneus', "Greek", []), \
-    # Character('Menelaus', "Greek", []), \ # not used in Lang 
-    Character('Menelaos', "Greek", []), \
+    Character('Idomeneus', "Greek", []), \
+    Character('Menelaos', "Greek", []), # Menelaus \
     Character('Nestor', "Greek", []), \
     Character('Odysseus', "Greek", []), \
     Character('Patroklos', "Greek", []), \
-    # Character('Phoenix', "Greek", []), \
-    # Character('Teucer', "Greek", []), \
-    # Character('Aesymnus', "Greek", []), \
+    Character('Phoinix', "Greek", []), #Phoenix \
+    Character('Teukros', "Greek", []), # Teucer, Teucros, Teucris, Teucris\
+    Character('Aisymnos', "Greek", []), #Aesymnos, Aesymnus\
 
     # Trojans
     #########
-    # Character('Aeneas', "Trojan", []), \ # not used in Lang 
-    # Character('Aineias', "Trojan", []), \ 
-    # Character('Agenor', "Trojan", []), \
-    # Character('Andromache', "Trojan", []), \
-    # Character('Antenor', "Trojan", []), \
-    # Character('Cassandra', "Trojan", []), \
-    # Character('Glaucus', "Trojan", []), \
+    Character('Aineias', "Trojan", []), # Aeneas\
+    Character('Agenor', "Trojan", []), \
+    Character('Andromache', "Trojan", []), \
+    Character('Antenor', "Trojan", []), \
+    Character('Cassandra', "Trojan", []), \
+    Character('Glaukos', "Trojan", []), # Glaucus \
     Character('Hector', "Trojan", []), \
-    # Character('Laodice', "Trojan", []),  \
-    # Character('Lycaon', "Trojan", []),  \
-    # Character('Pandarus', "Trojan", []),  \
+    Character('Laodike', "Trojan", []), # Laodice\
+    Character('Lykaon', "Trojan", []),  # Lycaon \
+    Character('Pandaros', "Trojan", []), # Pandarus \
     Character('Paris', "Trojan", []),  \
-    # Character('Polydamas', "Trojan", []),  \
+    Character('Polydamas', "Trojan", []),  \
     Character('Priam', "Trojan", []),  \
-    # Character('Sarpedon', "Trojan", []),  \
-    # Character('Theano', "Trojan", []),  \
-    # Character('Briseis', "Trojan", []),  \
+    Character('Sarpedon', "Trojan", []),  \
+    Character('Theano', "Trojan", []),  \
+    Character('Briseis', "Trojan", []),  \
 ]
 
 books = os.listdir('./lang')
